869818faac44c27cc01acd6d6a318eedf1171da7
David Martinez <davidmgilo@gmail.com>
2016-11-29 20:18:28 +0100

    test ok

--- tests/TaskControllerTest.php
+++ tests/TaskControllerTest.php
@@ -11,6 +11,22 @@ class TaskControllerTest extends TestCase
 {
     use DatabaseMigrations;
 
+    /*
+     * Overwrite createApplication to add Http Kernel
+     * see: https://github.com/laravel/laravel/pull/3943
+     *      https://github.com/laravel/framework/issues/15426
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
     /**
      * A basic test example.
      * @group todo
@@ -26,6 +42,7 @@ class TaskControllerTest extends TestCase
 
         $response = $this->call('GET', '/tasks');
 //        $response->content();
+//        dd($response);
         $this->assertEquals(200, $response->status());
 
         $this->assertViewHas('tasks');
