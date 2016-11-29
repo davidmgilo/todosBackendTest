3e8459cd05a69c5928d5f4763744831e31c9f7cd
David Martinez <davidmgilo@gmail.com>
2016-11-29 19:23:50 +0100

    added test for controller TDD

---
+++ tests/TaskControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Class TaskControllerTest
+ */
+class TaskControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     * @group todo
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
